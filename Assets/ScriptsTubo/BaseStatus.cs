2ed92db1b717ff7bd4e48da6ba385434cd3d064e
sota <sota.tubo@gmail.com>
2018-09-26 17:36:02 +0900

    拠点のステータス設定

---
+++ Assets/ScriptsTubo/BaseStatus.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseStatus : MonoBehaviour {
+
+	public int BaseHP;
+	public int BaseTag;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.tag == "EnemyUnit" && BaseTag == 1)
+		{
+			BaseHP -= 10; //本来は敵ユニットの攻撃力で判定する
+		}
+		else if (collision.gameObject.tag == "PlayerUnit" && BaseTag == 2)
+		{
+			BaseHP -= 10; //上に同じ
+		}
+		else if ((collision.gameObject.tag == "EnemyUnit" || collision.gameObject.tag == "PlayerUnit") && BaseTag == 3)
+		{
+			BaseHP -= 10;
+		}
+		else if ((collision.gameObject.tag == "EnemyUnit" || collision.gameObject.tag == "PlayerUnit") && BaseTag == 4)
+        {
+            BaseHP -= 10;
+        }
+	}
+}
---
+++ Assets/ScriptsTubo/enemyUnitMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyUnitMove : MonoBehaviour {
+
+	private GameObject neutBase, playerBase; //中立の拠点とプレイヤーの拠点
+
+	// Use this for initialization
+	void Start () {
+		neutBase = GameObject.FindWithTag("NeutralBase");
+		playerBase = GameObject.Find("PlayerBase");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
