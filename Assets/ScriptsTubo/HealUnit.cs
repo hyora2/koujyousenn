50ed3389da932feaa412c0ef99586a8e5b479dac
sota <sota.tubo@gmail.com>
2018-10-09 02:52:49 +0900

    モード切り替え時に正しく回復処理が行われていなかったので修正

--- Assets/ScriptsTubo/HealUnit.cs
+++ Assets/ScriptsTubo/HealUnit.cs
@@ -7,7 +7,7 @@ public class HealUnit : MonoBehaviour {
 	[SerializeField] private bool isPlayer; //このスクリプトがアタッチされているユニットがプレイヤーのユニットかどうか判定
 
 	private UnitStatus unitStatus, magicunitStatus; //magicunitStatusは魔法兵の回復力を参照している
-	private MagicHealMode healMode;
+	//private MagicHealMode healMode;
 	private GameObject magicunit;
 
 	// Use this for initialization
@@ -16,7 +16,7 @@ public class HealUnit : MonoBehaviour {
 		isPlayer = false;
 		magicunit = GameObject.Find("MagicUnit");
 		magicunitStatus = magicunit.GetComponent<UnitStatus>(); //プレイヤーのユニットか敵のユニットか区別できるようにする
-		healMode = magicunit.GetComponent<MagicHealMode>();
+		//healMode = magicunit.GetComponent<MagicHealMode>();
 	}
 	
 	// Update is called once per frame
@@ -30,7 +30,7 @@ public class HealUnit : MonoBehaviour {
 		{
 			if (trigger.gameObject == magicunit && magicunitStatus.unitCheck == true)
 			{
-				healMode.healcount++;
+				//healMode.healcount++;
 				unitStatus.unitHp += magicunitStatus.unitHealingpower;
 			}
 		}
@@ -38,7 +38,7 @@ public class HealUnit : MonoBehaviour {
 		{
 			if (trigger.gameObject == magicunit && magicunitStatus.unitCheck == false)
 			{
-				healMode.healcount++;
+				//healMode.healcount++;
 				unitStatus.unitHp += magicunitStatus.unitHealingpower;
 			}
 		}
