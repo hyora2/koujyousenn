1444ed3f168a81972484c18213e2cbc2ea59e4c5
sota <sota.tubo@gmail.com>
2018-10-08 16:21:31 +0900

    魔法兵回復モード実装

---
+++ Assets/ScriptsTubo/DEbug.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DEbug : MonoBehaviour {
+
+	private UnitStatus status;
+
+	// Use this for initialization
+	void Start () {
+		status = gameObject.GetComponent<UnitStatus>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//
+		Debug.Log(status.unitHp);
+	}
+}
