657a318a2bb62abfeb17bf5a8b8cd47612e340ad
DESKTOP-L041MKL\735se <735sezre9tf201x4@gmail.com>
2018-10-09 20:29:10 +0900

    両ドラッグ判定、向きの変更？

--- Assets/Units/UnitMove.cs
+++ Assets/Units/UnitMove.cs
@@ -3,18 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class UnitMove : MonoBehaviour {
-    public GameObject Line;
+    //public GameObject Line;
    
     int arrayi=0;
      int linepoint;
     float rx, ry;//進むべき方向
-    public float px , py ;// 引っ張り始めた点
+     float px , py ;// 引っ張り始めた点
     float fx, fy;//引っ張って離した点
     float sx, sy;//速度ベクトル
     float rate;
     public float speed=1;
     float rl;//進行方向ベクトルの大きさ
+    float sa;//x軸のポジションの差を図る
+    public float rotctl = 1;//回転具合
+    Vector2 fmouseposition;//前のマウスのポジション
     Vector2 pastpoint;
+    bool leftup=false;//左ボタンを離したか
     public float linespan=0.1f;
     Vector2 plusvec;
     
@@ -28,45 +32,68 @@ public class UnitMove : MonoBehaviour {
         lineRenderer = GetComponent<LineRenderer>();
         pastpoint = transform.position;
         plusvec = new Vector2(linespan, linespan);
-		
-	}
+        px = transform.position.x;
+        py = transform.position.y;
+        lineRenderer.positionCount = 0;
+        lineRenderer.SetPosition(0, transform.position);
+        Vector2 fmouseposition = transform.position;
+
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetMouseButtonUp(0))
         {
-           
-          
-            Move();
+            if (leftup == true)
+            {
+
+                Move();
+                leftup = false;
+            }
         }
       
     }
     private void OnMouseDrag()
     {
-
-        Vector2 pvec=pastpoint;
-        //引っ張る
         Vector2 currentScreenPoint = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
         Vector2 currentPisition = Camera.main.ScreenToWorldPoint(currentScreenPoint);
-        if ((Input.mousePosition.x - pastpoint.x) * -1 >= linespan) { plusvec.x = -linespan; }
-        else if ((Input.mousePosition.x - pastpoint.x)  >= linespan) { plusvec.x = linespan; }
-        else { plusvec.x = 0; }
-        if ((Input.mousePosition.y - pastpoint.y) * -1 >= linespan) { plusvec.y = -linespan; }
-        else if ((Input.mousePosition.y - pastpoint.y) >= linespan) { plusvec.y = linespan; }
-        else { plusvec.y = 0; }
-        
-       
 
-        if (Mathf.Abs(Input.mousePosition.x-pastpoint.x)>=linespan|| Mathf.Abs(Input.mousePosition.y- pastpoint.y) >= linespan) {
-            if (arrayi != 0) arrayi++;
-            pvec += plusvec;
-            // lineRenderer.SetVertexCount(arrayi);
-            lineRenderer.positionCount = arrayi;
-            lineRenderer.SetPosition(arrayi, pvec);
-            pastpoint = pvec;
+        if (Input.GetMouseButton(1))
+        {//向きの変更
+            sa = Input.mousePosition.x - fmouseposition.x;
+            transform.rotation = Quaternion.Euler(0, 0, sa * rotctl);
+            fmouseposition = Input.mousePosition;
         }
+        else if (Input.GetMouseButtonUp(1)){
+            return;
+        }
+        else
+        {
+            leftup = true;
+            Vector2 pvec = pastpoint;
+            //引っ張る
+
+            if ((Input.mousePosition.x - pastpoint.x) * -1 >= linespan) { plusvec.x = -linespan; }
+            else if ((Input.mousePosition.x - pastpoint.x) >= linespan) { plusvec.x = linespan; }
+            else { plusvec.x = 0; }
+            if ((Input.mousePosition.y - pastpoint.y) * -1 >= linespan) { plusvec.y = -linespan; }
+            else if ((Input.mousePosition.y - pastpoint.y) >= linespan) { plusvec.y = linespan; }
+            else { plusvec.y = 0; }
 
 
+
+            if (Mathf.Abs(Input.mousePosition.x - pastpoint.x) >= linespan || Mathf.Abs(Input.mousePosition.y - pastpoint.y) >= linespan)
+            {
+                arrayi++;
+                pvec += plusvec;
+                // lineRenderer.SetVertexCount(arrayi);
+                lineRenderer.positionCount = arrayi;
+                lineRenderer.SetPosition(arrayi, pvec);
+                pastpoint = pvec;
+            }
+
+        }
     }
 
     private void Move() {
