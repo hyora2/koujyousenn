d99c5958d4c55ee5951415cd0b51d009abdc86ea
MATH <mmath7786@gmail.com>
2018-10-30 17:50:18 +0900

    Revert "攻撃範囲の判定"
    This reverts commit 1c72e03d5f4da89832fd48ea1b5f803ca6642213.

--- Assets/Onishi/scripts/AttackRange.cs
+++ Assets/Onishi/scripts/AttackRange.cs
@@ -1,34 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-       
-public class AttckRange : MonoBehaviour {
-   
-    [SerializeField] private float range = 0.5f;
-    private UnitStatus unitStatus;
 
+public class NewBehaviourScript : MonoBehaviour {
+    
 	// Use this for initialization
 	void Start () {
-        GetComponent<CircleCollider2D>().radius = range;
-        unitStatus = gameObject.GetComponent<UnitStatus>();
+		
 	}
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        AttckRange attckRange = collision.gameObject.GetComponent<AttckRange>();
-        if(attckRange != null){
-            attckRange.Damage(unitStatus.unitPower);
-        }
-    }
-
-    // Update is called once per frame
-    void Update () {
-        
-      
+	
+	// Update is called once per frame
+	void Update () {
+		
 	}
-    public void Damage(int damage)
-    {
-        unitStatus.unitHp -= damage;
-    }
 }
-
-
